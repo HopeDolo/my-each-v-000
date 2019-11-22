88b2aca013ef726ac40fade72ca23cbec97941da
Darius Stewart <darius.stewart31@gmail.com>
2019-11-22 05:38:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,14 @@
 def my_each(array)
+  if block_given?
+    i = 0
 
-  
+    while i < array.length
+      yield(arrary[i])
+      i+= 1
+    end
+
+    array
+  else
+    puts "No block given"
+  end   
 end
